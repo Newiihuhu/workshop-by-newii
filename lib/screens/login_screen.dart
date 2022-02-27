6910f67ca6ce0d20b8fc9bbedf8d1ea26126b14d
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-02-27 15:06:32 +0700

    ⏳ add isLoading and spinkit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../constant.dart';
 import '../models/user_model.dart';
 import '../services/login_service.dart';
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -16,11 +18,19 @@ class _LoginScreenState extends State<LoginScreen> {
   String email = "";
   String password = "";
   late UserModel user;
+  bool isLoading = false;
 
   Future<UserModel> login(String email, String password) async {
-    this.user = await LoginService(email, password);
+    setState(() {
+      isLoading = true;
+    });
+    user = await LoginService(email, password);
+    setState(() {
+      isLoading = false;
+    });
     if (user.email.isNotEmpty) {
-      print("pass");
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
     } else {
       showDialog(
           context: context,
@@ -44,93 +54,106 @@ class _LoginScreenState extends State<LoginScreen> {
             );
           });
     }
-    return this.user;
+    return user;
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: size.height * 0.2),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Login",
-                style: TextStyle(color: lightColor, fontSize: 64),
-              )
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-            width: size.width * 0.8,
-            decoration: BoxDecoration(
-                color: secondaryColor, borderRadius: BorderRadius.circular(30)),
-            child: TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              cursorColor: primaryColor,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.person, color: primaryColor),
-                  hintText: "Your Email",
-                  border: InputBorder.none),
+    return isLoading
+        ? Center(
+            child: Container(
+            color: Colors.lightBlue[900],
+            child: SpinKitFadingCircle(
+              color: lightColor,
+              size: 50,
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-            width: size.width * 0.8,
-            decoration: BoxDecoration(
-                color: secondaryColor, borderRadius: BorderRadius.circular(30)),
-            child: TextFormField(
-              onChanged: (value) {
-                password = value;
-              },
-              cursorColor: primaryColor,
-              obscureText: showPassword,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.lock, color: primaryColor),
-                  suffixIcon: IconButton(
-                    onPressed: () {
+          ))
+        : Scaffold(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: size.height * 0.2),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Login",
+                      style: TextStyle(color: lightColor, fontSize: 64),
+                    )
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  width: size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextFormField(
+                    onChanged: (value) {
                       setState(() {
-                        showPassword = !showPassword;
+                        email = value;
                       });
                     },
-                    icon: Icon(
-                        showPassword ? Icons.visibility : Icons.visibility_off,
-                        color: primaryColor),
+                    cursorColor: primaryColor,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.person, color: primaryColor),
+                        hintText: "Your Email",
+                        border: InputBorder.none),
                   ),
-                  hintText: "Your Password",
-                  border: InputBorder.none),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  width: size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: TextFormField(
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    cursorColor: primaryColor,
+                    obscureText: showPassword,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.lock, color: primaryColor),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              showPassword = !showPassword;
+                            });
+                          },
+                          icon: Icon(
+                              showPassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: primaryColor),
+                        ),
+                        hintText: "Your Password",
+                        border: InputBorder.none),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  width: size.width * 0.8,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          login(email, password);
+                        },
+                        child: Text("LOGIN",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w500)),
+                        style: ElevatedButton.styleFrom(
+                            primary: primaryColor,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20)),
+                      )),
+                )
+              ],
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            width: size.width * 0.8,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: ElevatedButton(
-                  onPressed: () {
-                    login(email, password);
-                  },
-                  child: Text("LOGIN",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
-                  style: ElevatedButton.styleFrom(
-                      primary: primaryColor,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
-                )),
-          )
-        ],
-      ),
-    );
+          );
   }
 }
