cfc1fd21070bcd819f89e1a032ed425c94e6a66d
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-02-27 15:17:19 +0700

    😸 add webview to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -10,8 +11,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-    );
+    return SafeArea(
+        child: WebView(
+      initialUrl: 'https://github.com/Newiihuhu',
+    ));
   }
 }
